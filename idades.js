f3fb37ec393e8d2d1c1945b558ad4ee66072d05a
KAIKE JULIANO DA SILVA <kaike.js@puccampinas.edu.br>
2024-09-02 09:41:05 -0300

    *

---
+++ idades.js
@@ -0,0 +1,26 @@
+var idade = 79
+var nota = 10
+if (idade >= 18){
+    alert("Tué um adulto yayyyyyy paga imposto")
+
+
+}
+else {
+    alert("Não paga imposto ainda, não ajuda a patría! *Toca o hino nacional agressivamente*")
+}
+
+if (nota == 10){
+    alert("Galaxy Brain")
+
+}
+else if (nota >=7 && nota < 10){
+    alert("Aprovado")
+}
+
+else if(nota >= 5 && nota < 7 ){
+    alert("Recuperação")
+}
+
+else{
+    alert("Reprovado")
+}
